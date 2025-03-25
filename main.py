2f4dcb26f23a563fdb1bcdec4cc8dc93c201d87f
Ved Patel <talk2ved11@gmail.com>
2025-03-25 16:07:38 -0400

    Refactor virtual camera streaming: implement threading for stream control and handle graceful shutdown on interrupt

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from load import stream_to_virtual_cam
 import threading
 import time
 
+stop_event = threading.Event()
 
-virtual_cam_thread = threading.Thread(target=stream_to_virtual_cam, daemon=True)
-virtual_cam_thread.start()
+virtual_cam_thread = threading.Thread(
+    target=stream_to_virtual_cam, args=(stop_event,), daemon=True
+)
 
-time.sleep(5)
+try:
+    virtual_cam_thread.start()
+    print(f"Virtual Camera Thread ID: {virtual_cam_thread.ident}")
 
-virtual_cam_thread.stop()
+    time.sleep(5)
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt detected. Stopping thread...")
+    stop_event.set()
+except Exception:
+    stop_event.set()
+finally:
+    virtual_cam_thread.join(timeout=2)
+    print("Thread stopped.")
